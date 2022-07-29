d443fb52bc1cb2f3b7965ad5c8b153e6e26e0c53
lucasmessias9898 <36428045+lucasmessias9898@users.noreply.github.com>
2022-07-28 21:46:38 -0300

    add material UI e iniciado a regra das linhas

--- ptps/src/App.jsx
+++ ptps/src/App.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Box, FormControlLabel, Grid, Radio, RadioGroup, ThemeProvider, Typography } from "@mui/material";
+
+import theme from './style/index'
+import { Container } from "@mui/system";
 
 function App() {
+
+    const [linha, setLinha] = useState('561')
+
   return (
-    <div className="app">
-      <h1>Hello World!</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="app">
+        <Container>
+            <Grid item sm={12} xs={12} display='flex' justifyContent={'center'}>
+                <Box mt={2} mb={2}>
+                    <Typography variant="h2">
+                        Selecione uma linha:
+                    </Typography>
+                </Box>
+            </Grid>
+            <Grid item sm={12} xs={12} display='flex' justifyContent={'center'}>
+                <RadioGroup row defaultValue={linha} onChange={(e) => setLinha(e.target.value)}>
+                    <FormControlLabel value={linha && '561'} control={<Radio />} label="Linha 561" />
+                    <FormControlLabel value={linha && '562'} control={<Radio />} label="Linha 562" />
+                </RadioGroup>
+            </Grid>
+        </Container>
+      </div>
+      </ThemeProvider>
   );
 }
 
---
+++ ptps/src/style/index.js
@@ -0,0 +1,96 @@
+import { createTheme } from '@mui/material';
+
+
+export default createTheme({
+    spacing : 4,
+    palette: {
+        primary	: {
+            main : '#2D2A26',
+        },
+    },
+    typography : {
+        fontFamily : '"Roboto", sans-serif',
+        color : '#2D2A26',
+        h1 : {
+            fontSize : '3rem',
+            lineHeight : '3.5rem',
+            fontWeight: 'bold'
+        },
+        h2 : {
+            fontSize : '2rem',
+            lineHeight : '2.5rem',
+            fontWeight: 'bold'
+        },
+        h3 : {
+            fontSize : '1.5rem',
+            lineHeight : '1.625rem',
+            fontWeight: 'bold'
+        },
+        body1 : {
+            fontSize : '1rem',
+            lineHeight : '1.5rem',
+            margin : 0,
+            color : '#2D2A26'
+        }
+    },
+    overrides: {
+        MuiCssBaseline: {
+            '@global': {
+                '*' : {
+                    fontFamily : '"Roboto", sans-serif',
+                },
+                '*::-webkit-scrollbar': {
+                    width: '8px',
+                    height: '8px',
+                },
+                '*::-webkit-scrollbar-track': {
+                    boxShadow: 'inset 0px 0px 6px #00000029',
+                    border: '0',
+                },
+                '*::-webkit-scrollbar-thumb': {
+                    backgroundColor: '#EAEAEA',
+                    outline: '0px',
+                    border: '0px',
+                },
+                '@font-face': [
+                    {
+                        fontFamily		: 'Roboto',
+                        fontStyle		: 'normal',
+                        fontWeight		: 400,
+                        fontDisplay		: 'swap',
+                    },
+                    {
+                        fontFamily		: 'Roboto',
+                        fontStyle		: 'normal',
+                        fontWeight		: 700,
+                        fontDisplay		: 'swap',
+                    },
+                    {
+                        fontFamily		: 'Roboto',
+                        fontStyle		: 'normal',
+                        fontWeight		: 700,
+                        fontDisplay		: 'swap',
+                    },
+                    {
+                        fontFamily		: 'Roboto',
+                        fontStyle		: 'italic',
+                        fontWeight		: 700,
+                        fontDisplay		: 'swap',
+                    },
+                    {
+                        fontFamily		: 'Roboto',
+                        fontStyle		: 'normal',
+                        fontWeight		: 300,
+                        fontDisplay		: 'swap',
+                    },
+                    {
+                        fontFamily		: 'Roboto',
+                        fontStyle		: 'normal',
+                        fontWeight		: 500,
+                        fontDisplay		: 'swap',
+                    }
+                ],
+            },
+        },
+    },
+});
